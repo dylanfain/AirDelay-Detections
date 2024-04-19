95ead58c53c65d57a6a49ceda83e8048afe1ef3d
SAMIR SALDANHA <Samir.Saldanha@andorhealth.com>
2024-04-19 17:54:28 -0400

    edited together both mains see if works

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include "sort.h"
 #include "GUI.h"
 
+bool dropdownContains(const sf::Text& item, const sf::Vector2f& pos);
+void handleDropdownSelection(bool& dropdownOpen, const std::vector<sf::Text>& dropdownItems, const std::vector<std::string>& options, const sf::Vector2f& mousePosition, sf::Text& dropdownText);
+
 std::vector<FlightData> filterByWeatherDelay(const std::vector<FlightData>& flightData, bool hasWeatherDelay) {
     std::vector<FlightData> filteredData;
     for (const auto& flight : flightData) {
@@ -40,7 +43,7 @@ int main() {
     //Parsing file data Flight_delay.csv
     vector<FlightData> flightData = parseCSV("files/Flight_delay.csv");
 
-//    //example of all functions in use for final results
+/*    //example of all functions in use for final results
 //    //example filters added to flightdata
 //    flightData = filterByAirline(flightData, "American Airlines Inc.");
 //    flightData = filterByWeatherDelay(flightData, false);
@@ -81,7 +84,7 @@ int main() {
 //        std::cout << "Weather Delay: " << flight.weatherDelay << std::endl;
 //    }
 //    // Output execution time
-//    std::cout << "Merge sort execution time: " << duration2.count() << " milliseconds" << std::endl;
+    std::cout << "Merge sort execution time: " << duration2.count() << " milliseconds" << std::endl;*/
 
 
 
@@ -95,9 +98,9 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Dropdown Menu");
     gui test;
     // Dropdown menu options
-    std::vector<std::string> options = {"Option 1", "Option 2", "Option 3"};
+    //std::vector<std::string> options = {"Option 1", "Option 2", "Option 3"};
     //dropdown menu rectangle
-    sf::RectangleShape dropdownRect = test.Rect(50, 50, 30, 120);
+    //sf::RectangleShape dropdownRect = test.Rect(50, 50, 30, 120);
 
     //Font can be moved into gui class if needed
     sf::Font font;
@@ -106,74 +109,9 @@ int main() {
         return 1;
     }
     //creates dropdown text, position is based on center not corner
-    sf::Text dropdownText = test.text(110,65, 10,"Select an option", font);
+    //sf::Text dropdownText = test.text(110,65, 10,"Select an option", font);
     // Dropdown menu items
-    std::vector<sf::Text> dropdownItems = test.dropdown(options, dropdownRect, font);
-
-    // Dropdown menu state
-    bool dropdownOpen = false;
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-
-            if (event.type == sf::Event::MouseButtonReleased) {
-                if (dropdownRect.getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y))) {
-                    dropdownOpen = !dropdownOpen;
-                } else if (dropdownOpen) {
-                    for (size_t i = 0; i < dropdownItems.size(); ++i) {
-                        if (dropdownItems[i].getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y))) {
-                            std::cout << "Selected: " << options[i] << std::endl;
-                            //sets text
-                            dropdownText.setString(dropdownItems[i].getString());
-                            dropdownOpen = false;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        window.clear(sf::Color::White);
-
-        // Draw dropdown menu rectangle and text
-        window.draw(dropdownRect);
-        window.draw(dropdownText);
-
-        // Draw dropdown menu items if open
-        if (dropdownOpen) {
-            for (const auto& item : dropdownItems) {
-                window.draw(item);
-            }
-        }
-
-        window.display();
-    }
-
-    return 0;
-}
-
-//ANDREW
-
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <vector>
-#include "GUI.h"
-
-bool dropdownContains(const sf::Text& item, const sf::Vector2f& pos);
-void handleDropdownSelection(bool& dropdownOpen, const std::vector<sf::Text>& dropdownItems, const std::vector<std::string>& options, const sf::Vector2f& mousePosition, sf::Text& dropdownText);
-
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Dropdown Menu");
-    gui test;
-    sf::Font font;
-    if (!font.loadFromFile("files/font.ttf")) {
-        std::cerr << "Failed to load font" << std::endl;
-        return 1;
-    }
+    //std::vector<sf::Text> dropdownItems = test.dropdown(options, dropdownRect, font);
 
     // Titles and Labels
     sf::Text title("AirDelay Detections", font, 24);
